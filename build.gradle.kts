2dc37a3f6d5385564e9f53d5263b4141f6a219aa
Mariusz Sołtysiak <mariusz.soltysiak@sumup.com>
2024-06-02 09:19:38 +0200

    Fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-jdbc")
 
 	implementation("org.flywaydb:flyway-core")
+	implementation("org.flywaydb:flyway-database-postgresql")
 	runtimeOnly("org.postgresql:postgresql")
 
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
